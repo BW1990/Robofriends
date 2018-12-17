be03f6880b74a25b1624633f084a2433834d316a
Brian Winward <bwinward@mackenzieinvestments.com>
2018-12-17 15:58:39 -0500

    Updates

---
+++ precache-manifest.719d2488a17393d939311c1d43829a8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc0ab6a9afbc4cea91f3",
+    "url": "/Robofriends/static/css/main.738412ba.chunk.css"
+  },
+  {
+    "revision": "bc0ab6a9afbc4cea91f3",
+    "url": "/Robofriends/static/js/main.bc0ab6a9.chunk.js"
+  },
+  {
+    "revision": "a876aa8c8086242f1d66",
+    "url": "/Robofriends/static/css/1.c15e599b.chunk.css"
+  },
+  {
+    "revision": "a876aa8c8086242f1d66",
+    "url": "/Robofriends/static/js/1.a876aa8c.chunk.js"
+  },
+  {
+    "revision": "39a3a35c71e0df973673",
+    "url": "/Robofriends/static/js/runtime~main.39a3a35c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "d88c4dc6d2753bf925bd24e816ef0745",
+    "url": "/Robofriends/index.html"
+  }
+];
